6e78f90514938e0bd9a480d085d4bc15ae1fdc96
minnnnnnni <dkstodtjdals@gmail.com>
2025-07-10 03:06:47 +0900

    feat/models.py 수정 및 마이그레이션

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.timezone import now
+from moments.models import Moment  
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True) 
+    moment_id = models.ForeignKey(Moment, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_date = models.DateTimeField(default=now)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"[{self.moment_id}][{self.comment_id}] {self.user_id.nickname} - {self.content[:20]}"
+    # 아래의 형식으로 출력하는 함수
+    # [2][7] 닉네임 - 댓글 내용입니다. 
