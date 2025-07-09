55458374cf46944f402fd3102cb34aa7634781e4
minnnnnnni <dkstodtjdals@gmail.com>
2025-07-09 17:43:57 +0900

    feat/path 설정

--- moments/urls.py
+++ moments/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from .views.image_views import images_by_moment, delete_image
+from .views.moment_views import moment_root, moment_detail, moment_update, moment_delete
 
 urlpatterns = [
+    # 글 작성 path
+    path('', moment_root, name='moment_root'),          # POST /moments, GET /moments (글 생성, 목록 조회[카테고리별])
+    path('<int:moment_id>/', moment_detail, name='moment_detail'),      # GET /moments/<id> (개별 게시글 조회)
+    path('<int:moment_id>/', moment_update, name='moment_update'),      # PUT /moments/<id>
+    path('<int:moment_id>/', moment_delete, name='moment_delete'),      # DELETE /moments/<id>
+
+
+    # 이미지 관련 path
     path('<int:moment_id>/images/', images_by_moment, name='images_by_moment'), # 이미지 기본 CRUD는 글별 전체 이미지를 처리
     path('<int:moment_id>/images/<int:image_id>/', delete_image) # 삭제는 특정 이미지도 가능
 ]
