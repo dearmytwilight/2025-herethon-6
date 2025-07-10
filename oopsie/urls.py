84c505af3d1c588fb1fbbba25b905a5c28a49700
baseballclub <abkimdoyo25@gmail.com>
2025-07-10 23:05:34 +0900

    오탈자 수정

--- oopsie/urls.py
+++ oopsie/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('moments/', include('moments.urls')),
     path('main/', views.main_view, name='main'), 
-    path('pages/moments/', include('moments.pages_urls')) # 페이지 렌더는 pages/로 시작!
+    path('pages/moments/', include('moments.pages_urls')), # 페이지 렌더는 pages/로 시작!
     path('main/', views.main_view, name='main'), 
 ]
