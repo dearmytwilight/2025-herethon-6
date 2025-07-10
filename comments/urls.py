996f94d3744bb1429dd53e2b3b9c10ea354bbb9e
minnnnnnni <dkstodtjdals@gmail.com>
2025-07-10 12:14:07 +0900

    feat/댓글 views.py crud 기능

--- comments/urls.py
+++ comments/urls.py
@@ -3,5 +3,5 @@ from .views import comment_root, comment_detail_root
 
 urlpatterns = [
     path('', comment_root, name='comment_root'),                            # GET, POST
-    path('<int:comment_id>/', comment_detail_root, name='comment_detail'),  # PUT, DELETE
+    path('<int:comment_id>/', comment_detail_root, name='comment_detail'),  # POST(PUT 기능), DELETE
 ]
