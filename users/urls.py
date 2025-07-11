3d813422a7c2ccff4a3042344507bdc83ca12f17
baseballclub <abkimdoyo25@gmail.com>
2025-07-11 20:39:58 +0900

    mypage 연동

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views.login import login_view
 from .views.signup import signup_view
+from .views.mypage import mypage_view
 
 app_name = "users"
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
-
+    path('mypage/', mypage_view, name='mypage')
 ]
